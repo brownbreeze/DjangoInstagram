205b2266cd1091e84885aceed0e8c46bc8af6a02
brwonbreeze <913breeze@gmail.com>
2023-03-22 15:31:49 +0000

    create base
    - static/ 기본 jquery 버전 추가
       - wget https://code.jquery.com/jquery-3.4.1.min.js
    - templates/layout.html templates폴더 추가를 위한 기본 html
    - settings.py역할의 common.py보완
       - static, media, tempaltes 경로 설정

--- dhcompany/settings/common.py
+++ dhcompany/settings/common.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'dhcompany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'dhcompany', 'templates')    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'dhcompany', 'static')   
+]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
