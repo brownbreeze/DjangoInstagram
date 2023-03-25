41ca8563afcaa4c43edf9ccada34cf53f0245dfe
brwonbreeze <913breeze@gmail.com>
2023-03-25 03:13:50 +0000

    create base
    - urls debug_toolbar 적용

--- dhcompany/urls.py
+++ dhcompany/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings 
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
+    # MEDIA파일에 대한 스태틱 서브 파일 ?
+    urlpatterns += static(settings.MEDIA_URL,
+                            document_root=settings.MEDIA_ROOT)
+    
