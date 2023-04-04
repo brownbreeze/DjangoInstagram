1e8361a12376f0b1099645a0afed9ff8148f316b
brwonbreeze <913breeze@gmail.com>
2023-04-04 13:19:09 +0000

    create signup views
    - _form.html 분리
       - include _form.html with 구문 사용
    - django-bootstrap4 사용

--- dhcompany/settings/common.py
+++ dhcompany/settings/common.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
+    'bootstrap4',
     'debug_toolbar',
     # local apps 
     'accounts',
@@ -65,7 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'dhcompany', 'templates')    
+            os.path.join(BASE_DIR, 'dhcompany', 'templates'), 
             # templates 는 BASE_DIR 안에만 넣어도 됨. 굳이 dhcompany안에 넣지 안않아도 상관없음 
         ],
         'APP_DIRS': True,
