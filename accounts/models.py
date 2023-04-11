8a190defdff17c5bb04c5a975764c1d2a5d25365
brwonbreeze <913breeze@gmail.com>
2023-04-11 14:43:05 +0000

    create signup views
    - welcome email 메시지
    - render_to_string 을 이용 인자값 전달

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,23 @@
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
 from django.core.mail import send_mail
-
+from django.template.loader import render_to_string
 
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
     
     def send_welcome_email(self):
-        #title = ""
-        
-        pass
+        subject = render_to_string("accounts/welcome_email_subject.txt", {
+            "user":self,
+            
+        })
+        content = render_to_string("accounts/welcome_email_content.txt", {
+            "user":self,
+            
+        })
+        sender_email = settings.WELCOME_EMAL_SENDER
+        send_mail(subject, content, sender_email, [self.email], faile_silently=False)
+        #pass
         
         
