4268941a321ae2c16746d584d83c5224aa29fa13
brwonbreeze <913breeze@gmail.com>
2023-04-25 12:31:10 +0000

    create profile_edit view
    - phone_numbr 정규식 반영
    - gender 선택지
        - models.TextChoices
    - makemigrations 수행

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,20 @@
+from django.conf import settings 
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
 #from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.core.validators import RegexValidator
 
 class User(AbstractUser):
+    # django3 
+    class GenderChoices(models.TextChoices):
+        male = "M", "남성"
+        female = "F", "여성"
+    
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True, validators=[RegexValidator(r"^010[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
     
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {
