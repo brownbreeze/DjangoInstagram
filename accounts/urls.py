6821305963e4a45c3cd0a47ce0707510d75f4ae5
brwonbreeze <913breeze@gmail.com>
2023-04-26 14:56:33 +0000

    create profile_edit view
    - PasswordChangeView 활용
    - 비밀번호변경 화면 구성

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login, name='login'), # /accounts/login -> settings.LOGIN_URL
     path('logout/', views.logout, name='logout'), 
     path('signup/', views.signup, name='signup'),
+    path('password_change/', views.password_change, name='password_change'),
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
