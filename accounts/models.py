aada353fc79d3e67f6669d6b3d7488e827f8b1f6
brwonbreeze <913breeze@gmail.com>
2023-05-03 13:55:39 +0000

    create instagram Post view
    - 게시물 옆,상단바 avatar 수정
       - property 이용, 없을 경우 pyidenticon 사용

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 #from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.core.validators import RegexValidator
+from django.shortcuts import resolve_url
 
 class User(AbstractUser):
     # django3 
@@ -21,7 +22,14 @@ class User(AbstractUser):
     @property
     def name(self):
         return f'{self.first_name} {self.last_name}'
-    
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return resolve_url("pydenticon_image", self.username)
+
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {
             "user":self,
