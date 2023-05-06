8f36999070a339d98c673e14f6542d5d25332f85
brwonbreeze <913breeze@gmail.com>
2023-05-06 17:18:57 +0000

    create user page
    - 화면 구성
       - 사진 photo.url추가
       - url regex 수정 > 사진 띄우기 오류 수정

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,5 +8,5 @@ app_name = 'instagram'
 urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    re_path(r'^(?P<username>[\w.0+-]+)/', views.user_page, name='user_page')
+    re_path(r'^(?P<username>[\w.0+-]+)/$', views.user_page, name='user_page')
 ]
