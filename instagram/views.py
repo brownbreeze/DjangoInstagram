35c9825e593b259455f71f30dcdcab9f0965272f
brwonbreeze <913breeze@gmail.com>
2023-05-20 15:56:35 +0000

    change Post
    - CommentForm 생성
        - comment_form.html 정의
    - comment_new 생성

--- instagram/views.py
+++ instagram/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .models import Post
 
 @login_required
@@ -73,6 +73,23 @@ def post_unlike(request, pk):
     messages.success(request, f"{post.pk}를 좋아요를 취소합니다.")
     redirect_url = request.META.get("HTTP_REFEER", "root")
     return redirect(redirect_url)        
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect(comment.post)
+    else:
+        form = CommentForm()
+    return render(request, "instagram/comment_form.html",{
+        "form" : form,
+    })
 
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)    
